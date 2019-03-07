9c0dd93520d102ba149efb92e058261cbc860627
hyunsukimsokcho <hskimsokcho@gmail.com>
2019-03-07 16:02:47 +0900

    [hyunsu-cleaner] Add cleaner component and timer

--- Component/cleaner.py
+++ Component/cleaner.py
@@ -1,10 +1,23 @@
-
+import pandas as pd
+import numpy as np
 class Cleaner:
 
     def __init__(self, **kwargs):
-        pass
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+    
+    def literal_clean(self):
+        duplicate_ind_list = []
+        self.size, self.num_attr = self.data.shape
+        for i in range(self.size - 1):
+            for j in range(i+1, self.size):
+                if self.data.iloc[i].equals(self.data.iloc[j]):
+                    duplicate_ind_list.append(j)
 
-
+        self.data.drop(self.data.index[list(set(duplicate_ind_list))], inplace=True)
 
 if __name__ == "__main__":
-    print("Cleaner component implementation on progress")
+    print([range(4)]*3)
+    df = pd.DataFrame([range(4)]*3, columns=['A', 'B', 'C', 'D'])
+    cleaner = Cleaner(data=df)
+    cleaner.literal_clean()
