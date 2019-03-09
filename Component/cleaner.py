e1618a33589511aa107f07fdd1232b85e4398c45
hyunsukimsokcho <hskimsokcho@gmail.com>
2019-03-09 22:46:54 +0900

    [hyunsu-parallelCleaner] Implement parallelized cleaner

--- Component/cleaner.py
+++ Component/cleaner.py
@@ -1,34 +1,85 @@
 import pandas as pd
 import numpy as np
 import concurrent.futures
+
 class Cleaner:
 
     def __init__(self, **kwargs):
+        """
+        Constructor for cleaning component
+        """
         for key in kwargs:
             setattr(self, key, kwargs[key])
         setattr(self, 'size', self.data.shape[0])
     
-    def literal_clean(self, df1, i1, j1, df2=None, i2=None, j2=None):
+    def literal_clean(self, df1, df2=None, isPair=False, isParallel=False):
+        """
+        Cleans existing duplicate in data frame by pairwise comparison.
+
+        @description
+            It's behavior is different if it's part of parallel execution.
+            In such cases, it just returns the list of index to remove and 
+            deal with removal in parallel_clean scope
+
+        @params
+            df1: First data frame to compare with
+            df2: Explicit second data frame to compare with (If this is None, default is df1)
+            isParallel: boolean value indicating if this is part of parallel execution
+        """
         duplicate_ind_list = []
-        if df2 == None:
-            self.size, self.num_attr = self.data.shape
-            for i in range(i1, j1 - 1):
-                for j in range(i+1, j1):
-                    if self.data.iloc[i].equals(self.data.iloc[j]):
+        df1_list = df1.index.tolist()
+        if not isPair:
+            for i in df1_list[:-1]:
+                for j in df1_list[i+1:]:
+                    if df1.iloc[i].equals(df1.iloc[j]):
                         duplicate_ind_list.append(j)
         else:
-            pass
+            df2_list = df2.index.tolist()
+            for i in df1_list:
+                for j in df2_list:
+                    if df1.loc[i].equals(df2.loc[j]):
+                        duplicate_ind_list.append(j)
+        if isParallel:
+            return duplicate_ind_list
+        else:
+            if isPair:
+                df2.drop(list(set(duplicate_ind_list)), inplace=True)
+            else:
+                df1.drop(list(set(duplicate_ind_list)), inplace=True)
 
-        self.data.drop(self.data.index[list(set(duplicate_ind_list))], inplace=True)
+    def parallel_clean(self, n):
+        """
+        @description
+            Clean duplicate in parallelized fashion.
 
-    def batch_clean(self, n):
-
-        with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
+        @params
+            n: number of tasks that the entire work will be splitted into (for parallel exec.)
+        """
+        total_duplicate_ind_list = []
+        with concurrent.futures.ProcessPoolExecutor(max_workers=100) as executor:
             batch_jobs = []
             batch_size = int(np.floor(self.size / n))
+
+            for i in range(n-2):
+                for j in range(i, n-1):
+                    if i == j:
+                        batch_jobs.append(executor.submit(self.literal_clean, self.data[i*batch_size:(i+1)*batch_size], isParallel=True))
+                    else:
+                        batch_jobs.append(executor.submit(self.literal_clean, self.data[i*batch_size:(i+1)*batch_size], df2=self.data[j*batch_size:(j+1)*batch_size], isPair=True, isParallel=True))
+
             for i in range(n-1):
-                #batch_jobs.append(executor.submit(self.literal_clean, self.data[]))
-                pass
+                batch_jobs.append(executor.submit(self.literal_clean, self.data[i*batch_size:(i+1)*batch_size], df2=self.data[(n-1)*batch_size:], isPair=True, isParallel=True))
+
+            batch_jobs.append(executor.submit(self.literal_clean, self.data[(n-1)*batch_size:], isParallel=True))
+
+            for job in concurrent.futures.as_completed(batch_jobs):
+                if job.cancelled():
+                    print("[PARALLEL_CLEAN: JOB cancelled]: Check what's causing abnormal behavior")
+                    continue
+                elif job.done():
+                    job_result = job.result()
+                    total_duplicate_ind_list += job_result
+        self.data.drop(list(set(total_duplicate_ind_list)), inplace=True)
 
 
 
@@ -38,5 +89,5 @@ if __name__ == "__main__":
     df = pd.DataFrame([range(4)]*3, columns=['A', 'B', 'C', 'D'])
     cleaner = Cleaner(data=df)
     print(cleaner.data)
-    cleaner.literal_clean(cleaner.data, 0, cleaner.size)
+    cleaner.literal_clean(cleaner.data)
     print(cleaner.data)
--- Component/duplicator.py
+++ Component/duplicator.py
@@ -50,11 +50,11 @@ class Duplicator:
             batch_jobs = []
             batch_size = int(np.floor(self.size / n))
             for i in range(n-1):
-                batch_jobs.append(executor.submit(self.add_raw_duplicate, self.train_data[i*batch_size:(i+1)*batch_size-1], k, parallel=True))
-            batch_jobs.append(executor.submit(self.add_raw_duplicate, self.train_data[(n-1)*batch_size:], k, parallel=True))
+                batch_jobs.append(executor.submit(self.add_raw_duplicate, self.train_data[i*batch_size:(i+1)*batch_size], k, isParallel=True))
+            batch_jobs.append(executor.submit(self.add_raw_duplicate, self.train_data[(n-1)*batch_size:], k, isParallel=True))
             for job in concurrent.futures.as_completed(batch_jobs):
                 if job.cancelled():
-                    print("[JOB cancelled]: Check what's causing abnormal behavior")
+                    print("[PARALLEL_DUPLICATE: JOB cancelled]: Check what's causing abnormal behavior")
                     continue
                 elif job.done():
                     job_result = job.result()
@@ -64,21 +64,23 @@ class Duplicator:
 
         #print(self.train_data)
             
-    def add_raw_duplicate(self, data, k, parallel=False):
+    def add_raw_duplicate(self, data, k, isParallel=False):
         """
         @description
             Generate duplicate of each tuple according to zipfian dist.
 
         @params
+            data: data frame to add raw duplicate
             k: maximum number of duplicates to generate according to zipfian dist. (k>1)
-            (As k increases, number of duplicates decreases. k=4 brings about 100 duplicates; HEURISTIC)
+            (As k increases, number of duplicates decreases. k=4 brings about 10% duplicates; HEURISTIC)
+            isParallel: boolean value indicating if this is part of parallel execution
         """
         self.size, self.num_attr = self.train_data.shape
         self.test_data = self.test_data.copy()
         total = 0
         data = data.reset_index(drop=True)
         data_len = data.shape[0]
-        if parallel:
+        if isParallel:
             data_dup_only = data[0:0]
 
         for i in range(data_len):
@@ -86,14 +88,14 @@ class Duplicator:
             # add n many duplicates into our current dataframe
             if n > 0:
                 dup = data.iloc[i]
-                if parallel:
+                if isParallel:
                     data_dup_only = data_dup_only.append(n*[dup], ignore_index=True)
                 else:
                     data = data.append(n*[dup], ignore_index=True)
                 
             total = total + n
         print("ADDED {} MANY RAW DUPLICATES".format(total))
-        if parallel:
+        if isParallel:
             return data_dup_only
         else: 
             return data
